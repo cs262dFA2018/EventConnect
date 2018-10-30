e1e5096f522030a21ad2d17ee218abd5990721bd
Theron Tjapkes <theron@zmagnet.com>
2018-10-30 10:53:42 -0400

    Add empty activity for creating an event

---
+++ app/src/main/java/edu/calvin/cs262/cs262d/eventconnect/AddEvent.java
@@ -0,0 +1,13 @@
+package edu.calvin.cs262.cs262d.eventconnect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+    }
+}
