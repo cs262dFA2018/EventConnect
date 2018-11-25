73081760de39975580480340edb4370aa247979e
Joey Faas <joeyfaas@gmail.com>
2018-11-24 21:06:50 -0500

    added the calendarView instead of having the subjects type in the date. Ran into a bug where they have to tap on the date field twice the first time they are in the filed, but once after that, need to investigate further

--- app/src/main/java/edu/calvin/cs262/cs262d/eventconnect/views/AddEvent.java
+++ app/src/main/java/edu/calvin/cs262/cs262d/eventconnect/views/AddEvent.java
@@ -1,12 +1,18 @@
 package edu.calvin.cs262.cs262d.eventconnect.views;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import edu.calvin.cs262.cs262d.eventconnect.R;
 import edu.calvin.cs262.cs262d.eventconnect.data.Event;
@@ -14,17 +20,55 @@ import edu.calvin.cs262.cs262d.eventconnect.data.MockDatabase;
 
 public class AddEvent extends AppCompatActivity {
     private EditText eventTitle, eventDescription, eventHost, eventDate, eventLocation, eventCost, eventThreshold, eventCapacity;
+    Calendar calendar = Calendar.getInstance();
 
+    // initialize a DatePickerDialog set to name date for the onClickListener
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+        /**
+         *
+         * @param view
+         * @param year
+         * @param monthOfYear
+         * @param dayOfMonth
+         */
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, monthOfYear);
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+    };
 
+    /**
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
         //access the UI Edit Texts
+
         eventTitle = (EditText) findViewById(R.id.title);
         eventDescription = (EditText) findViewById(R.id.description);
         eventHost = (EditText) findViewById(R.id.host);
         eventDate = (EditText) findViewById(R.id.date);
+        // onClick listener for eventDate to pull up the calendar widget
+        eventDate.setOnClickListener(new View.OnClickListener() {
+            /**
+             * onClick for activating the calendar widget
+             * once the date EditText is clicked
+             * @param view
+             */
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(AddEvent.this, date,
+                        calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
         eventLocation = (EditText) findViewById(R.id.location);
         eventCost = (EditText) findViewById(R.id.cost);
         eventThreshold = (EditText) findViewById(R.id.threshold);
@@ -45,6 +89,12 @@ public class AddEvent extends AppCompatActivity {
     /*unless access to the settings activity is added from here,
      * onOptionItemSelected really needs to care about only the back arrow.
      */
+
+    /**
+     *
+     * @param item
+     * @return
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
@@ -54,13 +104,17 @@ public class AddEvent extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     *
+     * @param view
+     */
     public void onCreateEventClicked(View view) {
         //access the data from the UI elements
         //TODO: Enforce a title and description??
         String title = eventTitle.getText().toString();
         String desc = eventDescription.getText().toString();
         String loc = eventLocation.getText().toString();
-        String date = eventDate.getText().toString();
+        //String date = eventDate.getText().toString();
         String host = eventHost.getText().toString();
         Event event = new Event();
 
@@ -69,7 +123,7 @@ public class AddEvent extends AppCompatActivity {
         catch (java.lang.NumberFormatException e) {cost = 0;}
 
         int threshold;
-        int capacity;
+        final int capacity;
 
         //TODO: prevent user from putting illegal threshold and capacity in.
         try {threshold = (int) Math.floor(Double.parseDouble(eventThreshold.getText().toString()));
@@ -127,7 +181,7 @@ public class AddEvent extends AppCompatActivity {
         }
         catch (RuntimeException e){}
 
-        event.setDate(date);
+        //event.setDate(date);
         event.setCost(cost);
 
         //access and update the database.
@@ -137,5 +191,15 @@ public class AddEvent extends AppCompatActivity {
         finish();
     }
 
+    /**
+     * This method updates the onClick listener for the calendar
+     * widget, setting it to the MM/dd/yy format and Local US date
+     **/
+    private void updateLabel(){
+        String DateFormat = "MM/dd/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(DateFormat, Locale.US);
+        eventDate.setText(sdf.format(calendar.getTime()));
+
+    }
 
 }
